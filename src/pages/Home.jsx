c7d2a6995b6551bbfd8345c3fbd3d5591e9847e4
Aluno <Aluno@151ET1109560.catolica.ubec>
2026-04-28 12:40:06 -0300

    Integra logos das marcas na Home utilizando BrandCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import Header from '../components/Header'
 import BrandCard from '../components/BrandCard'
 
+import adidasLogo from '../assets/adidas.png'
+import nikeLogo from '../assets/nike.png'
+import gucciLogo from '../assets/gucci.png'
+import armaniLogo from '../assets/armani.png'
+
 function Home() {
   return (
     <>
@@ -13,10 +18,10 @@ function Home() {
         gap: "10px",
         padding: "20px"
       }}>
-        <BrandCard name="adidas" />
-        <BrandCard name="nike" />
-        <BrandCard name="gucci" />
-        <BrandCard name="armani" />
+        <BrandCard name="adidas" image={adidasLogo} />
+        <BrandCard name="nike" image={nikeLogo} />
+        <BrandCard name="gucci" image={gucciLogo} />
+        <BrandCard name="armani" image={armaniLogo} />
       </div>
     </>
   )
