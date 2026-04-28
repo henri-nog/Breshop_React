e5834c46bebad521c28825492481afa31e68ecbb
Aluno <Aluno@151ET1109560.catolica.ubec>
2026-04-28 12:20:20 -0300

    Adiciona controle de abertura do Navbar no Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,31 @@
+import { useState } from 'react'
+import Navbar from './Navbar'
+
 function Header() {
+  const [open, setOpen] = useState(false)
+
   return (
-    <header style={{
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-      padding: "15px 20px",
-      borderBottom: "1px solid #ccc"
-    }}>
-      
-      <h2>NetClothes</h2>
+    <>
+      <header style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "15px 20px",
+        borderBottom: "1px solid #ccc"
+      }}>
+        
+        <h2>NetClothes</h2>
 
-      <div>
-        <button>🔍</button>
-        <button>👤</button>
-        <button>☰</button>
-      </div>
+        <div>
+          <button>🔍</button>
+          <button>👤</button>
+          <button onClick={() => setOpen(true)}>☰</button>
+        </div>
 
-    </header>
+      </header>
+
+      <Navbar isOpen={open} onClose={() => setOpen(false)} />
+    </>
   )
 }
 
