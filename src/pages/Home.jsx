8bb602ff9430ef98460377ae3d5a6dc40195d032
Aluno <Aluno@151ET1109560.catolica.ubec>
2026-04-28 12:27:20 -0300

    Adiciona lista de marcas na Home utilizando BrandCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import Header from '../components/Header'
+import BrandCard from '../components/BrandCard'
 
 function Home() {
   return (
     <>
       <Header />
-      <h1>Página inicial</h1>
+
+      <h2 style={{ padding: "20px" }}>Marcas</h2>
+
+      <div style={{
+        display: "flex",
+        gap: "10px",
+        padding: "20px"
+      }}>
+        <BrandCard name="adidas" />
+        <BrandCard name="nike" />
+        <BrandCard name="gucci" />
+        <BrandCard name="armani" />
+      </div>
     </>
   )
 }
